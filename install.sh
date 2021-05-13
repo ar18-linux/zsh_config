9889b74b22799032ee72d59967e98fa855122dc4
nulysses <adr.zivil@gmail.com>
2021-05-13 08:51:47 +0200

    1

--- install.sh
+++ install.sh
@@ -17,11 +17,16 @@ if [ ! -d "${install_dir}" ]; then
 fi
 
 mkdir -p "${install_dir}"
-cp -f "${script_dir}/zsh_config/.zshrc" "${install_dir}/.zshrc"
-chown "$(logname):$(logname)" "${install_dir}/.zshrc"
-chmod 600 "${install_dir}/.zshrc"
+cp -f "${script_dir}/zsh_config/.zshrc" "/home/${user_name}/.zshrc"
+chown "${user_name}:${user_name}" "/home/${user_name}/.zshrc"
+chmod 600 "/home/${user_name}/.zshrc"
 
 mkdir "${script_dir}/build"
+
 cd "${script_dir}/build"
 git clone https://github.com/ar18-linux/libstderred.git
 "${script_dir}/build/libstderred/install.sh"
+
+cd "${script_dir}/build"
+git clone https://github.com/ar18-linux/zsh_ar18_lib.git
+"${script_dir}/build/zsh_ar18_lib/install.sh"
